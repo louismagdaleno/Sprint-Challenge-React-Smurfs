5e4b809235ac254b76f720dbb232c05218e3054a
Louis Magdaleno <buildthatapp@gmail.com>
2019-04-06 09:52:36 -0700

    STRETCH GOAL - Added DELETE smurf feature

--- village/src/App.js
+++ village/src/App.js
@@ -29,6 +29,12 @@ class App extends Component {
       smurfs : data
     })
   }
+
+  deleteSmurf(id){
+    Axios.delete(baseURL + `/smurfs/${id}`)
+    .then(response => this.setState({ smurfs : response.data }))
+    .catch(err => console.log(err));
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -42,7 +48,7 @@ class App extends Component {
             <li><NavLink to='/smurfForm' activeClassName="activeNavButton">Smurf Form</NavLink></li>
         </ul>
     
-        <Route exact path="/" render={() => <Smurfs smurfs={this.state.smurfs} />} />
+        <Route exact path="/" render={() => <Smurfs deleteSmurf={this.deleteSmurf} smurfs={this.state.smurfs} />} />
         <Route path="/smurfForm" render={() => <SmurfForm getSmurfs={this.getSmurfs} />} />
       </div>
     );
